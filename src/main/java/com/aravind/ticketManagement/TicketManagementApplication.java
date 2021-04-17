10ac6b7cdea7770cfb424f5a4e3a5c5a48479215
aravind mohan <aravind700529@gmail.com>
2021-04-17 09:52:59 +0530

    [MA] project Bootstrap

---
+++ src/main/java/com/aravind/ticketManagement/TicketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.aravind.ticketManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aravind/ticketManagement/TicketManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravind.ticketManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
