4718481418a39331ee3a1649b2c6ccc74b95c66f
aravind mohan <aravind700529@gmail.com>
2021-04-18 16:31:05 +0530

    [MA] Swagger Addition

--- src/main/java/com/aravind/ticketManagement/controller/TicketController.java
+++ src/main/java/com/aravind/ticketManagement/controller/TicketController.java
@@ -2,6 +2,10 @@ package com.aravind.ticketManagement.controller;
 
 import com.aravind.ticketManagement.model.Ticket;
 import com.aravind.ticketManagement.service.TicketService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Api(value="/ticketManagement",produces ="application/json")
 @RequestMapping(path="/ticketManagement")
 public class TicketController {
 
@@ -22,17 +27,32 @@ public class TicketController {
         ticketService=tktService;
     }
 
+    @ApiOperation(value="get All Tickets",response=Ticket[].class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="All Tickets Details Retrieved",response=Ticket[].class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/tickets", method= RequestMethod.GET)
     public ResponseEntity<List<Ticket>> findAll() {
         System.out.println(ticketService.getAllTickets().size());
         return new ResponseEntity<List<Ticket>>(ticketService.getAllTickets(), HttpStatus.OK);
     }
 
+    @ApiOperation(value="get Tickets By Id",response=Ticket.class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="specific Ticket Details",response=Ticket.class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/tickets/{id}", method= RequestMethod.GET)
     public Ticket findTicketById(@PathVariable int id) {
         return ticketService.findTicketById(id);
     }
 
+    @ApiOperation(value="get Tickets By AgentId",response=Ticket[].class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="AgentFiltered Tickets Details",response=Ticket[].class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/tickets/agent/{agentId}", method= RequestMethod.GET)
     public ResponseEntity<List<Ticket>> getTicketsByAgent(@PathVariable int agentId){
         List<Ticket> ticketsByAgent =ticketService.getAllTickets()
@@ -42,6 +62,11 @@ public class TicketController {
         return new ResponseEntity<List<Ticket>>(ticketsByAgent,HttpStatus.OK);
     }
 
+    @ApiOperation(value="get Tickets By CustomerId",response=Ticket[].class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="CustomerFiltered Tickets Details",response=Ticket[].class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/tickets/customer/{customerId}", method= RequestMethod.GET)
     public ResponseEntity<List<Ticket>> getTicketsByCustomer(@PathVariable int customerId){
         List<Ticket> ticketsByCustomer =ticketService.getAllTickets()
@@ -51,6 +76,11 @@ public class TicketController {
         return new ResponseEntity<List<Ticket>>(ticketsByCustomer,HttpStatus.OK);
     }
 
+    @ApiOperation(value="get Tickets By Status",response=Ticket[].class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="Status Filtered Tickets Details",response=Ticket[].class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/tickets/status/{status}", method= RequestMethod.GET)
     public ResponseEntity<List<Ticket>> getTicketsByStatus(@PathVariable String status){
         List<Ticket> ticketsByStatus =ticketService.getAllTickets()
@@ -60,11 +90,21 @@ public class TicketController {
         return new ResponseEntity<List<Ticket>>(ticketsByStatus,HttpStatus.OK);
     }
 
+    @ApiOperation(value="Create/Update Ticket details",response=String.class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="Ticket Saved Successfully",response=String.class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/ticket", method= RequestMethod.PUT)
     public int createOrUpdateTicket(@RequestBody Ticket ticket){
         return (ticketService.saveTicket(ticket));
     }
 
+    @ApiOperation(value="Update Status of a Ticket",response=String.class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="Ticket Status Updated Successfully",response=String.class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/ticket/{ticketId}/updateStatus/{status}", method= RequestMethod.PUT)
     public int updateStatus(@PathVariable int ticketId, Ticket.Status status){
         Ticket ticket=ticketService.findTicketById(ticketId);
@@ -72,6 +112,11 @@ public class TicketController {
         return (ticketService.saveTicket(ticket));
     }
 
+    @ApiOperation(value="Add Response to a Ticket",response=String.class)
+    @ApiResponses(value={
+            @ApiResponse(code=200,message="Ticket Responded Successfully",response=String.class),
+            @ApiResponse(code=500,message="Internal Server Error")
+    })
     @RequestMapping(value = "/ticket/{ticketId}/addResponse", method= RequestMethod.PUT)
     public void AddResponse(@PathVariable int ticketId,@RequestBody String response){
         ticketService.addResponse(ticketId,response);
